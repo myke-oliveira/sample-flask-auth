03d0a566a995769bcf3d74eb5d4589fd9f4a3fb3
Myke Albuquerque Pinto de Oliveira <mykeapo@gmail.com>
2024-07-13 23:04:18 +0000

    new: user model

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from models.user import User
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
+
+db = SQLAlchemy(app)
+# Session
 
 @app.route("/hello-world", methods=["GET"])
 def hello_world():
